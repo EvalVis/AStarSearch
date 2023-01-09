e284fdd30e8e836adff66fec246fc64a7757387a
Evaldas Visockas <vievaldas@gmail.com>
2023-01-09 20:30:16 +0200

    Generalize inversion distance heuristic

--- Puzzle15/src/main/java/ev/projects/astar/heuristics/IDorMDHeuristic.java
+++ Puzzle15/src/main/java/ev/projects/astar/heuristics/IDorMDHeuristic.java
@@ -2,12 +2,17 @@ package ev.projects.astar.heuristics;
 
 import ev.projects.heuristics.AStarHeuristic;
 import ev.projects.heuristics.AStarObject;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class IDorMDHeuristic extends AStarHeuristic<int[]> {
+
+    private final int puzzleLineSize;
+
     @Override
     public int calculateValue(AStarObject<int[]> aStarObject) {
         int MD = new ManhattanDistanceHeuristic().calculateValue(aStarObject);
-        int ID = new InversionDistanceHeuristic().calculateValue(aStarObject);
+        int ID = new InversionDistanceHeuristic(puzzleLineSize).calculateValue(aStarObject);
         return Math.max(MD, ID);
     }
 }
--- Puzzle15/src/main/java/ev/projects/astar/heuristics/InversionDistanceHeuristic.java
+++ Puzzle15/src/main/java/ev/projects/astar/heuristics/InversionDistanceHeuristic.java
@@ -3,15 +3,21 @@ package ev.projects.astar.heuristics;
 import ev.projects.heuristics.AStarHeuristic;
 import ev.projects.heuristics.AStarObject;
 import ev.projects.utils.Utils;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class InversionDistanceHeuristic extends AStarHeuristic<int[]> {
+
+    private final int puzzleLineSize;
+
     @Override
     public int calculateValue(AStarObject<int[]> aStarObject) {
         int[] cells = aStarObject.getCurrentStateData();
         int verticalInversions = Utils.getInversionCountLeftRight(cells);
         int horizontalInversions = Utils.getInversionCountTopBottom(cells);
-        int verticalDistance = verticalInversions % 3 + verticalInversions / 3;
-        int horizontalDistance = horizontalInversions % 3 + horizontalInversions / 3;
+        int maxInversionsNumber = puzzleLineSize - 1;
+        int verticalDistance = verticalInversions % maxInversionsNumber + verticalInversions / maxInversionsNumber;
+        int horizontalDistance = horizontalInversions % maxInversionsNumber + horizontalInversions / maxInversionsNumber;
         return verticalDistance + horizontalDistance;
     }
 }
